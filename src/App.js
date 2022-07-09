c8e184efe27d2429c0b1275f50c6b0ce4730d5db
Akarshgu <akarshgupta705415@gmail.com>
2022-07-09 16:15:36 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import  Navigation from './componet/Navigation'
+import CardDetailsPage from './Pages/CardDetailsPage'
+import Contact from './Pages/Contact'
+import Fav from './Pages/Fav'
+import Homes from './Pages/Homes'
+import Order from './Pages/Order'
+import Profile from './Pages/Profile'
 
-function App() {
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path='/' element = {<Homes /> } />
+        <Route path='/Home' element = {<Homes /> } />
+        <Route path='/contect' element = {<Contact /> } />
+        <Route path='/fav' element = {<Fav /> } />
+        <Route path='/order' element = {<Order /> } />
+        <Route path='/profile' element = {<Profile /> } />
+        <Route path=':category/:id' element = {<CardDetailsPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
